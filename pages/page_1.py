9656bf71b4ea306d9bb9a20522c113ef58b299a0
allanaasilva <allana.silva@ihm.com.br>
2024-06-21 19:00:22 -0300

    nuvens de palavras e histograma cmc

--- pages/page_1.py
+++ pages/page_1.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 from src.scryfall import ScryfallHandler
 from src.visualizations import vis_commander_by_released_date
+from src.visualizations import vis_cmd_distribuition
+from src.visualizations import vis_word_cloud
 from src.visualizations import add_logo
 
 
@@ -16,8 +18,37 @@ def get_data():
 
 df_commander_cards = get_data()
 st.header("Visão Commander")
-st.dataframe(df_commander_cards.head(10))
+# st.dataframe(df_commander_cards.head(10))
+
 st.plotly_chart(
     vis_commander_by_released_date(df_commander_cards),
     use_container_width=True)
+st.plotly_chart(vis_cmd_distribuition(df_commander_cards))
 
+
+
+st.header("Nuvem de Palavras")
+input_type = st.selectbox("Escolha nuvem de palavras que gostaria de exibir:",
+                          ['Carta Texto', 'Palavras-chave'],
+                          help="""Selecione o tipo de nuvem de palavras que
+                          deseja visualizar.""")
+
+if input_type == 'Palavras-chave':
+    modulo = st.selectbox("Escolha como gostaria que as palavras-chave fossem"
+                          " exibidas:",
+                          ['Módulos Combinados', 'Módulos Separados'],
+                          help="""Em alguns casos, há mais de uma palavra-chave
+                          por módulo. Por isso, escolha se prefere que elas
+                          sejam tratadas em conjunto, ou individualmente!""")
+
+    # Exibi a nuvem de palavras apenas após a seleção do usuário
+    if st.button("Exibir nuvem de palavras"):
+        # Nuvem de palavras
+        image_buffer = vis_word_cloud(df_commander_cards, 'Palavras-chave',
+                                      modulo)
+        st.image(image_buffer, use_column_width=True)
+
+else:
+    if st.button("Exibir nuvem de palavras"):
+        image_buffer = vis_word_cloud(df_commander_cards, 'Carta Texto', None)
+        st.image(image_buffer, use_column_width=True)
--- src/visualizations.py
+++ src/visualizations.py
@@ -1,8 +1,16 @@
+import io
 import plotly.graph_objects as go
+import plotly.express as px
 import streamlit as st
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 
 def add_logo():
+    st.set_page_config(
+        page_title="MTG - Uma análise sobre seu Deck",
+        page_icon="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/microsoft/106/mage_1f9d9.png"
+)
     st.markdown(
         """
         <style>
@@ -43,11 +51,64 @@ def vis_commander_by_released_date(df_commander_cards):
                     yaxis_title='Quantidade')
     return fig
 
-def vis_cmd_distribuition():
-    pass
 
-def vis_word_cloud():
-    pass
+def vis_cmd_distribuition(df):
+
+    fig = px.histogram(df,
+                       x='cmc',
+                       title = ('Distribuição de Custo de Mana Convertido '
+                                '(CMC) dos Comandantes'),
+                       labels={'cmc':'CMC'},
+                       opacity=0.8,
+                       color_discrete_sequence=['indianred'])
+    fig.update_layout(yaxis_title="Quantidade")
+
+    return fig
+
+
+def vis_word_cloud(df, col_name: str, sep_keywords: str):
+    if col_name == 'Palavras-chave':
+        if sep_keywords == 'Módulos Combinados':
+            all_keywords_combined = ' '.join([' '.join(keywords) for keywords
+                                              in df['keywords']])
+            # Gerar a nuvem de palavras combinada
+            wordcloud_combined = WordCloud(width=800, height=400,
+                                           background_color='white').generate(
+                                               all_keywords_combined)
+            # Plotar a nuvem de palavras combinada
+            plt.figure(figsize=(10, 5))
+            plt.imshow(wordcloud_combined, interpolation='bilinear')
+            plt.axis('off')
+
+        else:
+            individual_keywords = ' '.join([keyword for keywords in
+                                            df['keywords'] for keyword in
+                                            keywords])
+            wordcloud_individual = WordCloud(
+                width=800, height=400, background_color='white').generate(
+                    individual_keywords)
+            # Plotar a nuvem de palavras individual
+            plt.figure(figsize=(10, 5))
+            plt.imshow(wordcloud_individual, interpolation='bilinear')
+            plt.axis('off')
+
+    else:
+        all_text = ' '.join(df['oracle_text'].dropna())
+        wordcloud = WordCloud(width=800,
+                              height=400,
+                              background_color='white').generate(all_text)
+        # Plotar a nuvem de palavras
+        plt.figure(figsize=(10, 5))
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis('off')
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+
+    # Buffer de memória contendo a imagem
+    return buf
+
 
 def vis_edhrec_rank():
     pass
